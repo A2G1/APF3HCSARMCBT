beaefdae9b7a13dcc5c2001e8a32ab4d0c30a128
Andy Greer <andersonagme@gmail.com>
2022-11-13 17:37:21 -0700

    finished Greer's about me page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,22 +9,17 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className='title'>Congratulations! You've set up your webapp!</h1>
+        <h1 className='title'>Welcome to our group website!</h1>
 
         <p className='description'>
-          Get started by editing <code>src/pages/index.js</code> to be your
-          project home page.
-        </p>
-        <p className='description2'>
-          Next, each team member will create their own about me file in the
-          pages directory:{' '}
+          Created by Andy Greer, Andy Gusty, and George Frommell
         </p>
         <code>src/page/teamMemberName.js</code>
 
         <div className='grid'>
-          <a href='/ronBurgundy' className='card'>
-            <h3>Example About Me Page &rarr;</h3>
-            <p>Click here to navigate to an example page</p>
+          <a href='/andyGreer' className='card'>
+            <h3>About Andy Greer</h3>
+            <p>Click here to navigate to Andy Greer's page</p>
           </a>
           <a
             href='https://github.com/wangalec/website-template'
